603a10888e08bf86d0e699ba42677ef73cd839d6
Daniel Szabo <dgszabo@gmail.com>
2018-04-12 13:30:11 -0700

    merge conflict

--- fun-with-props/src/App.js
+++ fun-with-props/src/App.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
 import Person from './Person';
-=======
->>>>>>> 911a6b9eb42d0034451590bc09f2e2ea3738f493
 
 class App extends Component {
   render() {
@@ -17,12 +14,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-<<<<<<< HEAD
         <Person name="John" favColor="red"/>
         <Person name="Jane" favColor="yellow"/>
         <Person name="Tiffany" favColor="teal"/>
-=======
->>>>>>> 911a6b9eb42d0034451590bc09f2e2ea3738f493
       </div>
     );
   }
